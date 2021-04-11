d4797c2930ed056e93d2c2a8756506638a839921
Marsunpaisti <marsunpaisti@gmail.com>
2021-04-11 19:08:03 +0300

    Fix ardy cloak teleport in webwalker

--- paistisuite/paistisuite.gradle.kts
+++ paistisuite/paistisuite.gradle.kts
@@ -1,4 +1,4 @@
-version = "2.3.4"
+version = "2.3.5"
 
 project.extra["PluginName"] = "PaistiSuite"
 project.extra["PluginDescription"] = "Scripting framework by Paisti. Required by all other Paisti Plugins to work!"
